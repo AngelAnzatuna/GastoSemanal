578ae53099260cb0a769a2c95d18c455aa4afd3c
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-20 18:30:04 +0100

    Agregar función para imprimir alertas y validar campos en el formulario de gastos

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 // Clases
@@ -25,6 +27,29 @@ class UI {
         // Agregando al HTML
         document.querySelector('#total').textContent = presupuesto;
         document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo){
+        // Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if(tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // Insertar en el HTML
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        // Quitar del HTML
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
     }
 }
 
@@ -46,4 +71,25 @@ function preguntarPresupuesto() {
     console.log(presupuesto);
 
     ui.insertarPresupuesto(presupuesto);
+}
+
+// Añade gastos
+function agregarGasto(e) {
+    e.preventDefault();
+
+    // Leer los datos del formulario
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    // Validar
+    if(nombre === '' || cantidad === '') {
+        ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+        return;
+    } else if(cantidad <= 0 || isNaN(cantidad)) {
+        ui.imprimirAlerta('Cantidad no valida', 'error');
+        return;
+    }
+
+    console.log('Agregando gasto');
+
 }
