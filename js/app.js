a6feed6477494fec3edd22294ac4d4c7389faf6e
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-21 18:51:25 +0100

    Agregar función para listar gastos y limpiar el HTML previo

--- js/app.js
+++ js/app.js
@@ -56,6 +56,41 @@ class UI {
             divMensaje.remove();
         }, 3000);
     }
+
+    agregarGastoListado(gastos) {
+        
+        this.limpiarHTML(); // Elimina el HTML previo
+
+        // Iterar sobre los gastos
+        gastos.forEach(gasto => {
+            const {cantidad, nombre, id} = gasto;
+
+            // Crear un Li
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id;
+
+            // Agregar el HTML del gasto
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill"> ${cantidad} </span>`;
+
+            // Boton para borrar el gasto
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML = `Borrar &times`;
+            nuevoGasto.appendChild(btnBorrar);
+            
+            // Agregar al HTML
+            gastoListado.appendChild(nuevoGasto);
+
+        })
+        
+    }
+
+    limpiarHTML() {
+        while(gastoListado.firstChild) {
+            gastoListado.removeChild(gastoListado.firstChild);
+        }
+    }
 }
 
 // Instanciar
@@ -103,6 +138,10 @@ function agregarGasto(e) {
 
     // Mensaje todo bien
     ui.imprimirAlerta('Gasto Agregado Correctamente');
+
+    // Imprimir los gastos
+    const{gastos} = presupuesto;
+    ui.agregarGastoListado(gastos);
 
     // Reinicia el formulario
     formulario.reset()
